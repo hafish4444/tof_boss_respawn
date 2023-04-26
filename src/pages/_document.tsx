cbe9ccc416333d7649321b5da5c5cc588993c446
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-04-26 17:42:27 +0700

    [Update] title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>TOF Boss Respawn</title>
+        <meta property="og:title" content="TOF Boss Respawn" key="title" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="all" />
       </Head>
       <body>
         <Main />
