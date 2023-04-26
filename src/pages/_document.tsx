a371473a40d39b95e30c519d5ac06ee9c53d6889
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-04-26 17:39:20 +0700

    [Add] title web

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TOF Boss Respawn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="all" />
+      </Head>
       <body>
         <Main />
         <NextScript />
