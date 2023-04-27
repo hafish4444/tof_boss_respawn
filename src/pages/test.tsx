c6f47598b72327775e98aff75300c3247685e593
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-04-27 10:36:22 +0700

    [Add] test page (for mongodb)

---
+++ src/pages/test.tsx
@@ -0,0 +1,47 @@
+import clientPromise from '../../lib/mongodb'
+import { InferGetServerSidePropsType } from 'next'
+
+export async function getServerSideProps(context: any) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+export default function Home({
+  isConnected,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="container">
+      <main>
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
+        </h1>
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+      </main>
+    </div>
+  )
+}
