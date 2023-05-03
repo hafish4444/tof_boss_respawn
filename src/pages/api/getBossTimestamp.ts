2f543146a85bbdb52bb5a6da079f1c85b8a1b7c4
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-05-03 17:35:44 +0700

    [Update] getBossRespawn 10 minutes

--- src/pages/api/getBossTimestamp.ts
+++ src/pages/api/getBossTimestamp.ts
@@ -21,7 +21,7 @@ export default async function handler(req :any, res: any) {
                 {
                   $gte: [
                     { $toDate: "$respawnTime" },
-                    { $toDate: moment().subtract(30, 'minutes').toDate() }
+                    { $toDate: moment().subtract(10, 'minutes').toDate() }
                   ]
                 },
                 bossIds.length 
