32a6da81af94e65f82cca9c76cd8a9b769a95f3f
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-06-15 16:53:56 +0700

    [Add] useAnalytics of happykit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useAnalytics } from '@happykit/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Track page views
+  useAnalytics({ publicKey: "analytics_pub_2e5826eb38" })
   return <Component {...pageProps} />
 }
