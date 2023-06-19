c7001e41828a32b708396ce389e66b5fe1aa23a8
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-06-19 12:45:09 +0700

    [Remove] 1 cron

--- src/pages/api/migrate/bossTimestamp.ts
+++ src/pages/api/migrate/bossTimestamp.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import moment from "moment";
 import clientPromise from "../../../../lib/mongodb";
+import fs from 'fs';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -32,6 +33,57 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       await db.collection("boss_time_stamp_temp").insertMany(bossTimeStamp);
       const documentIds = bossTimeStamp.map(doc => doc._id);
       await db.collection("boss_time_stamp").deleteMany({ _id: { $in: documentIds } });
+
+      const bossTimeStampTemp = await db
+      .collection("boss_time_stamp_temp")
+      .aggregate([
+        {
+          $match: {
+            $expr: {
+              $and: [
+                {
+                  $lt: [
+                    { $toDate: "$respawnTime" },
+                    { $toDate: moment().startOf('day').add(4, 'hour').subtract(1, 'days').toDate() }
+                  ]
+                }
+              ]
+            }
+          }
+        },
+        {
+            $sort: {
+                createdBy: -1
+            }
+        },
+        { 
+            $group: { 
+                _id: '$createdBy', 
+                count: { 
+                    $sum: 1
+                }
+            }
+        },
+        {
+            $project: { 
+                userId: '$_id', 
+                userName: '', 
+                count: 1, 
+                _id: 0 
+            } 
+        }
+      ])
+      .toArray();
+
+      // Save the JSON data to a file
+      fs.writeFile('data/userList.json', JSON.stringify(bossTimeStampTemp), (err) => {
+          if (err) {
+              console.error(err);
+              res.status(500).json({ error: 'Unable to save JSON data' });
+          } else {
+              res.status(200).json({ message: 'JSON data saved successfully' });
+          }
+      });
     }
 
     (await clientPromise).close
