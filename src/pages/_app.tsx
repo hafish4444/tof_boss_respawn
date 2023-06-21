c55bc8a51eda0ad6c923cbdf61400c115056d4e5
CLICKNEXT\Abdunhafis.h <abdunhafis.h@clicknext.com>
2023-06-21 09:25:05 +0700

    [Update] Test analytics page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useAnalytics } from '@happykit/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   // Track page views
   useAnalytics({ publicKey: "analytics_pub_2e5826eb38" })
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
