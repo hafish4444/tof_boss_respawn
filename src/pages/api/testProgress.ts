45650578e83d684b4a1992670a9d9006c419c82a
DESKTOP-P2IS1TL\hafis <abdunhafis.h@clicknext.com>
2023-06-22 13:57:29 +0700

    [Update] testProgress for test table bossTimeStamp

--- src/pages/api/testProgress.ts
+++ src/pages/api/testProgress.ts
@@ -1,13 +1,48 @@
-
+import moment from "moment";
+import { NextApiResponse } from "next";
 import prisma from '../../../lib/prisma';
-import { NextApiRequest, NextApiResponse } from "next";
+interface InputProps {
+  bossList: [];
+  userId: string | undefined;
+  channel: number | undefined;
+  limit: number;
+  }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: { body: InputProps }, res: NextApiResponse) {
   try {
-    const feed = await prisma.users.findMany() 
-    res.status(200).json(feed);
+    let { bossList, userId, channel, limit } = req.body
+
+    const bossIds = bossList.length ? bossList : undefined
+
+    userId = userId || undefined;
+    channel = channel || undefined;
+
+    const bossTimeStamp = await prisma.boss_time_stamp.findMany({
+      where: {
+        isCheck: false,
+        createdBy: userId,
+        channel: channel,
+        respawnTime: {
+          gt: moment().subtract(15, 'minutes').toISOString(),
+        },
+        bosses: {
+          id: {
+            in: bossIds,
+          },
+        }
+      },
+      orderBy: {
+        dieTime: 'asc',
+      },
+      take: limit && limit > 0 ? limit : 40,
+      include: {
+        bosses: true,
+      }
+    });
+    
+    res.status(200).json(bossTimeStamp);
   } catch (e) {
-    console.error(e);
-    res.status(400).json({ msg: "Error retrieving users" });
+    console.log(e)
+    res.status(400).json({ msg: "Error retrieving boss_time_stamp", err: e });
   }
 }
